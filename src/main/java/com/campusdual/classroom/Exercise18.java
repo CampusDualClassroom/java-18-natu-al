fb28254fe321fd99783bee32e709444b62e00b50
Nataly Alvarado Lujan <nataly.al.luj@gmail.com>
2024-09-30 15:03:06 +0200

    No comprendo el problema con el método showInlineArray

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,32 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
+import java.util.Arrays;
+
 public class Exercise18 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        int n = Utils.integer("Introduce el tamaño del Array (número entero): ");
+        int[] a = createAndInitializeArray(n);
+        showInlineArray(a);
+    }
 
-	}
+    public static int[] createAndInitializeArray(int num) {
+        int[] array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+    public static void showInlineArray(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        //     System.out.print(Arrays.toString(a) + " ");
+    }
 }
+
+
